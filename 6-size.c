3d27820807e49eceb9237c9927ca69e3b51d58d9
Humera <humera210311@gmail.com>
2022-09-08 13:51:56 -0700

    My 6 commit

--- 6-size.c
+++ 6-size.c
@@ -5,20 +5,25 @@
 *
 *Description: using the main function
 *this program prints "Programming is like building a multilingual puzzle"
-*Return: 0
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-char c;
-int i;
-long li;
-long long lli;
-float f;
+char a;
 
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("size of a long int: %ld byte(s)\n", sizeof(li));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("Size of a float: %d byte(s)\n", sizeof(f));
+int b;
+
+long c;
+
+long long int d;
+
+float e;
+
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
+
 return (0);
 }
