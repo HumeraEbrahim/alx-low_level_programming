f2db23daf724112794af2446d5387ce908401d22
Humera <humera210311@gmail.com>
2022-09-08 13:17:26 -0700

    My 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
 *main - Prints the sizeof many var types
 *
-Return: Always 0 (Success)
+Return:Always 0 (Success)
 */
 int main(void)
 {
@@ -17,7 +17,7 @@ printf("Size of a char:%d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeofi(e));
 
 return (0);
 }
