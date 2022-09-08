d33a288939ccaa2e3d20c8cf9214975316b5eb3c
Humera <humera210311@gmail.com>
2022-09-08 11:15:11 -0700

    My 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* Desription: main-Prints out a string to stdout.
-* Return: 0 if success.
+*Desription: main-Prints out a string to stdout.
+*Return: 0 if success.
 */
 int main(void) 
 {
