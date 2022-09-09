0488410fb2de5f733e8432ed5744b14eb4182971
Humera <humera210311@gmail.com>
2022-09-09 08:52:09 -0700

    My 0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Description:print whether the number stored in the variable n is positive or negative.
+*Description:print whether number in the variable n is positive or negative.
 *
 *Always success
 */
