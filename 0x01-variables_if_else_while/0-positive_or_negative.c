d1dc3273e7fbd8b45f5ea8b6a7dbbc101cd3ead9
Humera <humera210311@gmail.com>
2022-09-09 08:32:37 -0700

    My 0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
 *main - Entry point
 *
-* Always success
+*Always success
 */
 int main(void)
 {
@@ -14,12 +14,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-        printf("%d is positive\n", n);
-else if (n == 0)
-        printf("%d is zero\n", n);
+if (n > 0)                                                           						printf("%d is positive\n", n);
+else if (n == 0)										     		printf("%d is zero\n", n);
 else if (n < 0)
-        printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 
 return (0);
 }
