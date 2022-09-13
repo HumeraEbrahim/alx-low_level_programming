ddfbeac18f536392cbf7bd988724fbf517fdc423
Humera <humera210311@gmail.com>
2022-09-13 08:00:13 -0700

    My 0 commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,14 @@
-#include <main.h>
+int -putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-/**
-*main - main block
-*Description: A program that prints _putchar
-*Return: 0 Success
-*/
-int main(void)
-{
-
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-int _putchar(char);
-
-		return (0)
-}
-
-
-
+#endif
