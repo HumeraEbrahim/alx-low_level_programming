bbcb4fcabc78dfbbf52254c316c1ac4f23283e6b
Humera <humera210311@gmail.com>
2022-09-13 04:21:31 -0700

    My 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main-main block
+*Description: A program that print _putchar
+*Return: 0 Success
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putcahr('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+		return (0);
+}
