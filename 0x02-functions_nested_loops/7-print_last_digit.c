006748bcb191b3ec7052223b4f70d94cc063b740
Humera <humera210311@gmail.com>
2022-09-13 13:44:48 -0700

    My 7 commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,17 @@
 
 /**
 *print_last_digit - Prints the last digit of a number.
-*@n: The number in question.
+*@n: takes 1n an integer
 *
 *Return: Value of the last digit.
 */
-int print_last_digit(int n)
+int print_last_digit(int)
 {
-	int (last_digit - n % 10);
+	int 1n = n % 10;
 
-	if (last_digit < 0)
-		last_digit *= -1;
+	if (n < 0)
+		1n = 1n* -1;
+	_putchar(1n + '0');
 
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+	return (1n);
 }
