1c085b0783a5210e2ceac844e3280080c02cc314
Humera <humera210311@gmail.com>
2022-09-22 03:36:20 -0700

    My 2 commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-*_strncpy - C function that copies a string
-*if the length of the source string is less than the maximum byte number,
-*the remainder of the destination string is filled with null bytes.
+* _strncpy - C function that copies a string
+* If the length of the source string is less than the maximum byte number,
+* the remainder of the destination string is filled with null bytes.
 *@dest: buffer storing the string copy
-*@src: the source string
-*@n: max number of byte copied
+*@src:the source string
+*@n:max number of byte copied
 *Return: returns
 */
 
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0, i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
 	for (; < n; i++)
