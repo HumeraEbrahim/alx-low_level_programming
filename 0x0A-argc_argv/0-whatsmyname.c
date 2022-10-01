f5bff00847ab58bf1f6ffe96404e19ab12801182
Humera <humera210311@gmail.com>
2022-10-01 07:51:22 -0700

    My 0 commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
 *main - print the program name
 *@argc: the number of arguments
-*@argv: An array of pointers
+*@argv: Arguments
 *
-*Return: Always 0
+*Return: Always 0 (Succes)
 */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+(void) argc;
 printf("%s\n", argv[0]);
+
 return (0);
 }
