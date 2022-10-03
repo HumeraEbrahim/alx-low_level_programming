b9bd0302ca3a082d54572bbd86a2d37fd7b8baa8
Humera <humera210311@gmail.com>
2022-10-03 10:40:42 -0700

    My 3 commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
 /**
 *main - prints the multiplication of two integers
 *@argc: argument count
-*argv: argument vector
+*@argv: argument vector
 *Return: 0 if true, 1 if false
 */
 int main(int argc, char *argv[])
