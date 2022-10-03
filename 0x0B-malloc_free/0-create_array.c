1259770ecdf0241a87a3d5583560976526a43235
Humera <humera210311@gmail.com>
2022-10-03 06:54:16 -0700

    My 0 commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *buffer;
-unigned int position;
+unsigned int position;
 
 if (size == 0)
 {
@@ -27,7 +27,7 @@ return (NULL);
 else
 {
 position = 0;
-while (position < ize) /*While for array*/
+while (position < size) /*While for array*/
 {
 *(buffer + position) = c;
 position++;
