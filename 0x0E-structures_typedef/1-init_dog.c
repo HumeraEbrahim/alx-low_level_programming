b230b294799c8030f14c14d5c63d2b82fa3975db
Humera <humera210311@gmail.com>
2022-10-10 08:08:15 -0700

    My 1 commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-*init_dog - Initializes a variable of type struct dog
+*init_dog - Initializes a variable of type struct dog.
 *@d: The dog to be initialized.
 *@name: The name of the dog.
 *@age: The age of the dog.
@@ -17,4 +17,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->owner = owner;
 	}
 }
-	
+
