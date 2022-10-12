5f1f17110d0f14b6ab6c442601899c67cb1aebdd
Humera <humera210311@gmail.com>
2022-10-12 04:58:45 -0700

    My 1 commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
 *array_literator - executes a function given as a parameter
 *on each element of an array
 *@array: array to iterate over
-*@size: of the array
+*@size: size of the array
 *@action: pointer to function used
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
