9d3a6f1ea51c3f70f490c498bfb896b515e8a2c0
Humera <humera210311@gmail.com>
2022-10-13 08:04:45 -0700

    My 2 commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,10 +27,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		if (i < n - 1)
 			if (separator)
-				print("%s", separator);
+				printf("%s", separator);
 	}
 
 	printf("\n");
 	va_end(valist);
 }
-
