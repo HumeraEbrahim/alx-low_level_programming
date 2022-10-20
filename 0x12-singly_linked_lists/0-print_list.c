ed4d1a4ed9f20eca2ccd64c20429afbfcdc7f332
Humera <humera210311@gmail.com>
2022-10-20 05:25:52 -0700

    MY 0 commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 *print_list - Prints all the elements of a list_t list.
 *@h: The list_t list.
 *
-*Return: The number of nodes in h.
+*Return: The number of elements in the list.
 */
 
 size_t print_list(const list_t *h)
