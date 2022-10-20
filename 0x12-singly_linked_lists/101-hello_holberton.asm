132ceab39138575e98c5b0d1d574a05c691ec667
Humera <humera210311@gmail.com>
2022-10-20 13:11:59 -0700

    My 6 commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -11,13 +11,13 @@ main:
    move rax,0
    call printf
 
-   pop rbp
+    pop rbp
 
     mov rax,0
     ret
 
 section .data
    msg: db "Hello, Hoberton", 0
-  fmt: db "%s", 10, 0
+   fmt: db "%s", 10, 0
 
 
