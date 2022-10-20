3be401dca0ddbccdb69ef10fcb01a1f942106ab8
Humera <humera210311@gmail.com>
2022-10-20 13:07:08 -0700

    My 6 commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+extern printf
+
+section .text
+	global main
+
+main:
+   push rbp
+
+
+   move rdi,fmt
+   move rsi,msg
+   move rax,0
+   call printf
+
+   pop rbp
+
+    mov rax,0
+    ret
+
+section .data
+   msg: db "Hello, Hoberton", 0
+  fmt: db "%s", 10, 0
+
+
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-5. The Hare and the Tortoise
+Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
