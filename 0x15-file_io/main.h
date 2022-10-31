908b008f17e40be9adc418005fd110f35401c1c0
Humera <humera210311@gmail.com>
2022-10-31 04:17:22 -0700

    My 0 commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <un1std.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
