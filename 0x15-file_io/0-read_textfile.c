00e473b51c8ac45cd55f5965fcbd15d6863e92dd
Humera <humera210311@gmail.com>
2022-10-31 04:00:36 -0700

    my 0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 *	function should read and print.
 *
 *Return: If the function fails or filename is NULL - 0.
-*	O/W - the actual number of bytes the function can read and print.
+*	O/w - the actual number of bytes the function can read and print.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -29,7 +29,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		close(o);
+		return (0);
+	}
 
-		return (w);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
