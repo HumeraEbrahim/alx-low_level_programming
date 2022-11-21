648380a2af9791f81ff7a261aa3fa9d0e2cb36da
Humera <humera210311@gmail.com>
2022-11-21 13:11:35 -0800

    My advanced commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+*main - Point of entry
+*
+*Return: Success 0
+*/
+
+int main(void)
+{
+	int k = '0';
+	int l = '0';
+
+	while (k <= '9')
+	{
+		while (l <= '9')
+		{
+			if (!(k > l || k == l))
+			{
+				putchar(k);
+				putchar(l);
+				if (k == '8' && l == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			l++;
+		}
+		l = '0';
+		k++;
+	}
+	return (0);
+}
